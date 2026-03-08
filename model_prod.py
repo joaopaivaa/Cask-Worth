9ca10d750bc0f2c3bc920564a76edce408d389bf
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2026-03-08 15:50:49 -0300

    adjusted model production file to use model base on data analysis

--- model_prod.py
+++ model_prod.py
@@ -1,24 +1,9 @@
 import pandas as pd
-import numpy as np
-import os
-
 from scipy.special import inv_boxcox
-
 from joblib import load
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-x_columns_corr_sel = os.getenv("x_columns_corr_sel").split(",")
-
-fitted_lambda_standard = float(os.getenv("fitted_lambda_standard"))
-fitted_lambda_per_rla = float(os.getenv("fitted_lambda_per_rla"))
-fitted_lambda_per_rla_times_age = float(os.getenv("fitted_lambda_per_rla_times_age"))
-
-scaler_x_standard = load('scalers/scaler_x_standard.pkl')
-scaler_x_per_rla = load('scalers/scaler_x_per_rla.pkl')
-scaler_x_per_rla_times_age = load('scalers/scaler_x_per_rla_times_age.pkl')
+fitted_lambda = load('variables_for_production_model/fitted_lambda.pkl')
+scaler_x = load('variables_for_production_model/scaler_x.pkl')
 
 models_metrics = pd.read_csv('models_metrics/models_metrics.csv')
 
@@ -35,34 +20,15 @@ def ensemble_model_predict(x_input):
 
         model = load(f'models/top{i+1}_model.pkl')
 
-        if (model_info['features'] == 'Correlation selected features'):
-            X = x_input[x_columns_corr_sel]
-        else:
-            X = x_input
+        X = x_input
 
-        if model_info['model'] in ['Linear Regression', 'Support Vector Regression']:
-            if (model_info['y_variable'] == 'Inflation Adjusted Hammer Price'):
-                X = scaler_x_standard.transform(X)
-            elif (model_info['y_variable'] == 'Inflation Adjusted Hammer Price per Litre of Alcohol'):
-                X = scaler_x_per_rla.transform(X)
-            elif (model_info['y_variable'] == 'Inflation Adjusted Hammer Price per Litre of Alcohol Times Age'):
-                X = scaler_x_per_rla_times_age.transform(X)
+        if model_info['model'] in ['Linear Regression', 'Support Vector Regression', 'Artificial Neural Network']:
+            X = scaler_x.transform(X)
 
         y_pred = model.predict(X)
 
-        if (model_info['transformation'] == 'Box-Cox transformation') and (model_info['y_variable'] == 'Inflation Adjusted Hammer Price'):
-            y_pred = inv_boxcox(y_pred, fitted_lambda_standard)
-        elif (model_info['transformation'] == 'Box-Cox transformation') and (model_info['y_variable'] == 'Inflation Adjusted Hammer Price per Litre of Alcohol'):
-            y_pred = inv_boxcox(y_pred, fitted_lambda_per_rla)
-        elif (model_info['transformation'] == 'Box-Cox transformation') and (model_info['y_variable'] == 'Inflation Adjusted Hammer Price per Litre of Alcohol Times Age'):
-            y_pred = inv_boxcox(y_pred, fitted_lambda_per_rla_times_age)
-        elif model_info['transformation'] == 'Log transformation':
-            y_pred = np.exp(y_pred)
-
-        if model_info['y_variable'] == 'Inflation Adjusted Hammer Price per Litre of Alcohol':
-            y_pred = y_pred * x_input['rla'].values
-        elif model_info['y_variable'] == 'Inflation Adjusted Hammer Price per Litre of Alcohol Times Age':
-            y_pred = y_pred * x_input['rla'].values / x_input['age'].values
+        y_pred = inv_boxcox(y_pred, fitted_lambda)
+        y_pred = y_pred * x_input['rla'].values
 
         y_pred_ensemble += y_pred
 
@@ -80,34 +46,15 @@ def best_model_predict(x_input):
 
     model = load(f'models/top1_model.pkl')
 
-    if (model_info['features'] == 'Correlation selected features'):
-        X = x_input[x_columns_corr_sel]
-    else:
-        X = x_input
+    X = x_input
 
-    if model_info['model'] in ['Linear Regression', 'Support Vector Regression']:
-        if (model_info['y_variable'] == 'Inflation Adjusted Hammer Price'):
-            X = scaler_x_standard.transform(X)
-        elif (model_info['y_variable'] == 'Inflation Adjusted Hammer Price per Litre of Alcohol'):
-            X = scaler_x_per_rla.transform(X)
-        elif (model_info['y_variable'] == 'Inflation Adjusted Hammer Price per Litre of Alcohol Times Age'):
-            X = scaler_x_per_rla_times_age.transform(X)
+    if model_info['model'] in ['Linear Regression', 'Support Vector Regression', 'Artificial Neural Network']:
+        X = scaler_x.transform(X)
 
     y_pred = model.predict(X)
 
-    if (model_info['transformation'] == 'Box-Cox transformation') and (model_info['y_variable'] == 'Inflation Adjusted Hammer Price'):
-        y_pred = inv_boxcox(y_pred, fitted_lambda_standard)
-    elif (model_info['transformation'] == 'Box-Cox transformation') and (model_info['y_variable'] == 'Inflation Adjusted Hammer Price per Litre of Alcohol'):
-        y_pred = inv_boxcox(y_pred, fitted_lambda_per_rla)
-    elif (model_info['transformation'] == 'Box-Cox transformation') and (model_info['y_variable'] == 'Inflation Adjusted Hammer Price per Litre of Alcohol Times Age'):
-        y_pred = inv_boxcox(y_pred, fitted_lambda_per_rla_times_age)
-    elif model_info['transformation'] == 'Log transformation':
-        y_pred = np.exp(y_pred)
-
-    if model_info['y_variable'] == 'Inflation Adjusted Hammer Price per Litre of Alcohol':
-        y_pred = y_pred * x_input['rla'].values
-    elif model_info['y_variable'] == 'Inflation Adjusted Hammer Price per Litre of Alcohol Times Age':
-        y_pred = y_pred * x_input['rla'].values / x_input['age'].values
+    y_pred = inv_boxcox(y_pred, fitted_lambda)
+    y_pred = y_pred * x_input['rla'].values
 
     y_pred = round(y_pred, 2)
 

b58cbe6ee8997438e6a7d9f135bc7775ed14a7b0
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2026-03-08 16:11:06 -0300

    adjusted dashboard to use model base on data analysis

--- cask_worth_dashboard.py
+++ cask_worth_dashboard.py
@@ -1,18 +1,12 @@
 import pandas as pd
 import streamlit as st
-import os
-from dotenv import load_dotenv
 
 from model_prod import cask_worth_predict
 
-load_dotenv()
+from joblib import load
 
-features = os.getenv("x_columns_all_features").split(",")
-features = [feature.replace('�', '') for feature in features]
-
-df_casks = pd.read_csv("database/casks_database__casks_valuation.csv")
-
-df_casks = df_casks[['auction_date', 'volume_12m', 'volume_6m', 'volume_3m']].drop_duplicates().sort_values('auction_date', ascending=False).head(1)
+x_columns_features = load('variables_for_production_model/x_columns_features.pkl')
+x_columns_features = [feature.replace('�', '') for feature in x_columns_features]
 
 st.set_page_config(layout='wide')
 
@@ -22,8 +16,8 @@ st.subheader('Whisky Casks Valuation')
 
 st.space('large') 
 
-col_age, col_strength, col_bulk_litres, col_cask_type = st.columns(4, vertical_alignment='center')
-col_region, col_country, col_distillery, col_previous_spirit = st.columns(4, vertical_alignment='center')
+col_age, col_strength, col_bulk_litres = st.columns(3, vertical_alignment='center')
+col_cask_type, col_cask_filling, col_distillery = st.columns(3, vertical_alignment='center')
 
 with col_age:
     age = st.number_input('Age', min_value=0, max_value=100, value=None, step=1)
@@ -36,73 +30,61 @@ with col_bulk_litres:
 
 with col_distillery:
     distillery = st.text_input('Distillery', value=None)
-    distillery = distillery.lower() if (distillery != None) else distillery
-
-with col_region:
-    region = st.text_input('Region', value=None)
-    region = region.lower() if (region != None) else region
-
-with col_country:
-    country = st.text_input('Country', value=None)
-    country = country.lower() if (country != None) else country
+    distillery = distillery.lower().replace(" ", "_") if (distillery != None) else distillery
 
 with col_cask_type:
     cask_type = st.text_input('Cask Type', value=None)
-    cask_type = cask_type.lower() if (cask_type != None) else cask_type
+    cask_type = cask_type.lower().replace(" ", "_") if (cask_type != None) else cask_type
 
-with col_previous_spirit:
-    previous_spirit = st.text_input('Previous Spirit', value=None)
-    previous_spirit = previous_spirit.lower() if (previous_spirit != None) else previous_spirit
+with col_cask_filling:
+    cask_filling = st.text_input('Cask Filling', value=None)
+    cask_filling = cask_filling.lower().replace(" ", "_") if (cask_filling != None) else cask_filling
 
 st.space('small')
 
 evaluate_button = st.button("Evaluate")
 
-vars_list = [age, strength, bulk_litres, distillery, region, country, cask_type, previous_spirit]
+vars_list = [age, strength, bulk_litres, distillery, cask_type, cask_filling]
 
 if (evaluate_button) and all(var is not None for var in vars_list):
 
     rla = bulk_litres * strength / 100
-    bottles_at_cask_strength = bulk_litres / 0.7
 
-    df = pd.DataFrame(columns=features)
-    df.loc[0, ] = [None] * len(features)
+    dim_distilleries = pd.read_csv('database/dimension/dim_distilleries_info.csv', sep=';')
 
-    distillery_columns = [f for f in features if "distillery" in f]
+    distillery_infos = dim_distilleries[dim_distilleries['Distillery'].str.lower().replace(" ", "_") == distillery]
+    region = distillery_infos['Region'].values[0] if len(distillery_infos) > 0 else None
+
+    df = pd.DataFrame(columns=x_columns_features)
+    df.loc[0, ] = [None] * len(x_columns_features)
+
+    distillery_columns = [f for f in x_columns_features if "distillery" in f]
     df[distillery_columns] = 0
 
-    region_columns = [f for f in features if "region" in f]
+    region_columns = [f for f in x_columns_features if "region" in f]
     df[region_columns] = 0
 
-    country_columns = [f for f in features if "country" in f]
-    df[country_columns] = 0
-
-    cask_type_columns = [f for f in features if "cask_type" in f]
+    cask_type_columns = [f for f in x_columns_features if "cask_type" in f]
     df[cask_type_columns] = 0
 
-    previous_spirit_columns = [f for f in features if "previous_spirit" in f]
-    df[previous_spirit_columns] = 0
+    cask_filling_columns = [f for f in x_columns_features if "cask_filling" in f]
+    df[cask_filling_columns] = 0
 
     df['age'] = age
     df['strength'] = strength
-    df['bulk_litres'] = bulk_litres
     df['rla'] = rla
-    df['bottles_at_cask_strength'] = bottles_at_cask_strength
-
-    df['volume_12m'] = df_casks['volume_12m'].values[0]
-    df['volume_6m'] = df_casks['volume_6m'].values[0]
-    df['volume_3m'] = df_casks['volume_3m'].values[0]
 
     if f'distillery_{distillery}' in df.columns:
         df[f'distillery_{distillery}'] = 1
+
     if f'region_{region}' in df.columns:
         df[f'region_{region}'] = 1
-    if f'country_{country}' in df.columns:
-        df[f'country_{country}'] = 1
+
     if f'cask_type_{cask_type}' in df.columns:
         df[f'cask_type_{cask_type}'] = 1
-    if f'previous_spirit_{previous_spirit}' in df.columns:
-        df[f'previous_spirit_{previous_spirit}'] = 1
+
+    if f'cask_filling_{cask_filling}' in df.columns:
+        df[f'cask_filling_{cask_filling}'] = 1
     
     casks_value = cask_worth_predict(df)
 
